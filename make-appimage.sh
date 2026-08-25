2144e7e5ae3114927cac8de04b94e00cc4dba34f
Link4Electronics <link4electronics@gmail.com>
2026-08-25 10:01:41 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -19,7 +19,3 @@ quick-sharun /usr/bin/kt* /usr/share/ktorrent /usr/lib/qt6/plugins/ktorrent_plug
 
 # Turn AppDir into AppImage
 quick-sharun --make-appimage
-
-# Test the app for 12 seconds, if the test fails due to the app
-# having issues running in the CI use --simple-test instead
-quick-sharun --test ./dist/*.AppImage
